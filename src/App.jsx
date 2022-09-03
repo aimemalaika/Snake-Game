d9d325a443564c4d5d435027ecab51b4241723d6
aime19 <>
2022-09-03 17:59:27 +0200

    My crontrib

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,103 @@
+import { useState, useEffect } from 'react';
 import SnakeBody from './components/snakebody';
-import { useState } from 'react';
+import useKeyPress from './hooks/usekeypress';
 import './App.css'
 
 export default function App() {
   const [snakeLength, setSnakeLength] = useState(1);
+
   const [foodPosition, setFoodPostion] = useState({
-    top: 0,
-    left: 0
+    top: `${Math.floor(Math.random() * 40) * 10}px`,
+    left: `${Math.floor(Math.random() * 32) * 10}px`
   });
-  let boxGrid = new Array(1353).fill().map((el, i) => <div key={i} className="table-pixel" />);
-  document.addEventListener('keydown', ({ keyCode }) => {
-    switch (keyCode) {
-      case 37:
-        console.log('send the snake to left')
-        break;
-      case 38:
-        console.log('send the snake to up')
-        break;
-      case 39:
-        console.log('send the snake to right')
-        break;
-      case 40:
-        console.log('send the snake to down')
-        break;
-      case 32:
-        console.log('pause the game')
-        break;
-      default:
-        e.preventDefault
-        break;
+  
+  const [snakePosition, setsnakePosition] = useState({
+    top: `${Math.floor(Math.random() * 40) * 10}px`,
+    left: `${Math.floor(Math.random() * 32) * 10}px`
+  });
+
+  const currentTop = parseInt(snakePosition.top);
+  const currentLeft = parseInt(snakePosition.left)
+
+  const keyUp = useKeyPress("ArrowUp");
+  const keyDown = useKeyPress("ArrowDown");
+  const keyLeft = useKeyPress("ArrowLeft");
+  const keyRight = useKeyPress("ArrowRight");
+
+  useEffect(() => {
+    if (keyLeft) {
+      if(currentLeft == 0) {
+        alert('Game over')
+      }else{
+        setsnakePosition(
+          {
+            top: `${currentTop}px`,
+            left: `${currentLeft - 10}px`
+          }
+        )
+      }
     }
-  });
+
+    if(keyUp) {
+      if(currentTop == 0) {
+        alert('Game over')
+      }else{
+        setsnakePosition(
+          {
+            top: `${currentTop - 10}px`,
+            left: `${currentLeft}px`
+          }
+        )
+      }
+    }
+
+    if(keyDown) {
+      if(currentTop == 400) {
+        alert('Game over')
+      }else{
+        setsnakePosition(
+          {
+            top: `${currentTop + 10}px`,
+            left: `${currentLeft}px`
+          }
+        )
+      }
+    }
+
+    if (keyRight) {
+      if(currentLeft == 320) {
+        alert('Game over')
+      }else{
+        setsnakePosition(
+          {
+            top: `${currentTop}px`,
+            left: `${currentLeft + 10}px`
+          }
+        )
+      }
+    }
+    
+    if (snakePosition.top == foodPosition.top && snakePosition.left == foodPosition.left) {
+      setSnakeLength(snakeLength + 1);
+      setFoodPostion({
+        top: `${Math.floor(Math.random() * 40) * 10}px`,
+        left: `${Math.floor(Math.random() * 32) * 10}px`
+      });
+    }
+
+  }, [keyLeft, keyRight, keyUp, keyDown])
+
+  let boxGrid = new Array(1353).fill().map((el, i) => < div key = {i} className = "table-pixel" / > );
+
   return (
     <div className="snake-box">
-      <div className="snake-box-grid">
-        {boxGrid}
+      <div className="snake-box-inner">
+        <div className="snake-box-grid">
+          {boxGrid}
+        </div>
+        <div className="snake-food" style={{ top: foodPosition.top, left: foodPosition.left }} /> 
+        <SnakeBody longueur={snakeLength} positions={snakePosition} />
       </div>
-      <div className="snake-food" data-top={foodPosition.top} data-left={foodPosition.left}/>
-      <SnakeBody longueur={snakeLength} />
     </div>
   )
 }
--- src/components/snakebody.jsx
+++ src/components/snakebody.jsx
@@ -1,7 +1,8 @@
-export default function SnakeBody({ longueur }) {
+export default function SnakeBody({ longueur, positions }) {
+
   let actualSkin = new Array(longueur).fill().map((el, i) => <div key={i} className="snake-skin" />)
   return (
-    <div className="snake-body">
+    <div className="snake-body" style={{ top: positions.top, left: positions.left }}>
       {actualSkin}
     </div>
   )
